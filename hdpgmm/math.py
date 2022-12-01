trying to fix the numerical instability issue of Cholesky decomposition

hdpgmm/math.py
@@ -56,7 +56,7 @@ def th_masked_logsumexp(
     keepdim: bool=False,
     mask: Optional[torch.Tensor]=None,
     max_offset_thrd: float=-1e10
-):
+) -> torch.Tensor:
     """ Numerically stable logsumexp on the last dim of `inputs`.
 
     reference: https://github.com/pytorch/pytorch/issues/2591
@@ -90,3 +90,73 @@ def th_masked_logsumexp(
     if not keepdim:
         outputs = outputs.squeeze(dim)
     return outputs
+
+
+def mat_sqrt(
+    A: torch.Tensor,
+    method: str = 'cholesky',
+    ensure_pos_semidef: bool = True,
+    eps: float = 1e-6,
+) -> tuple[torch.Tensor,
+           torch.Tensor,
+           torch.bool,
+           torch.float]:
+    """ Compute "square root" of the given symmetric matrix
+
+    It computes the "square root" of the given symmetric matrix. It provides
+    several methods, including the Cholesky decomposition (default), SVD,
+    and finally eigen decomposition. The implementation is based on `here`_.
+
+    Args:
+        A: input real symmetric matrix
+        method: specifying which method is used. {'cholesky', 'svd', 'eig'}
+        ensure_pos_semidef: if True, it tries to fix the issue by some tricks.
+                            however most of the case it should be used very carefully
+                            otherwise it'll just produce a degenerated outcome
+
+    Returns:
+        A: the input matrix A, which could be reconstructed from adjusted square root matrix of it
+        L: the decomposition of the input matrix which by computing the dot product
+           of itself can recover the input matrix
+        isposdef: boolean indicate the given matrix is positive semi definite
+        abslogdet: absolute log determinant computed from :obj:`torch.linalg.slogdet`
+
+    Raises:
+        ValueError if method is not supported
+
+    .. _here: https://nl.mathworks.com/matlabcentral/answers/225177-chol-gives-error-for-a-barely-positive-definite-matrix#answer_332354
+    """
+    if method not in {'cholesky', 'svd', 'eig'}:
+        raise ValueError(
+            '[ERROR] only `cholesky`, `svd` and `eig` methods are supported!'
+        )
+
+    if method == 'cholesky':
+        L, info = torch.linalg.cholesky_ex(A)
+        is_bad = info.sum() != 0
+        if is_bad and ensure_pos_semidef:
+            L, info = torch.linalg.cholesky_ex(A + eps * torch.eye(A.shape[-1]))
+
+    elif method == 'eig':
+        U, d = torch.linalg.eig(A)
+        bads = d.float() < 0.
+        is_bad = bads.sum() > 0
+        if is_bad and ensure_pos_semidef:
+            d[d.float() < 0] = 0.
+        L = torch.linalg.qr(torch.diag(torch.sqrt(d)) @ U.mT).R.mT
+
+    elif method == 'svd':
+        U, s, _ = torch.linalg.svd(A)
+        bads = s < 0.
+        is_bad = bads.sum() > 0.
+        if is_bad and ensure_pos_semidef:
+            s[s < 0] = 0.
+        L = torch.bmm(U, torch.diag_embed(torch.sqrt(s)))
+
+    if ensure_pos_semidef:
+        A = torch.bmm(L, L.mT)
+    sign, logabsdet = torch.linalg.slogdet(A)
+    if any(sign <= 0):
+        raise ValueError('[ERROR] log det of W must be positive!')
+
+    return A, L, is_bad, logabsdet

hdpgmm/parameters.py
@@ -64,6 +64,10 @@ class NormalWishartParameters(Parameters):
             contains :math:`E_{q}[\\eta]`, which follows (Blei and Jordan 2006)
         Eq_a_eta (:obj:`numpy.typing.NDArray`):
             contains :math:`E_{q}[a(\\eta)]`, which follows (Blei and Jordan 2006)
+        stable (bool): with this flag, the "square root" of the precision matrix W
+                       is computed using the SVD with forcefully truncating the
+                       eigenvalues larger or equal to zero. If not set, it is
+                       computed by the Cholesky decomposition.
     """
     mu0: npt.NDArray[np_float]
     lmbda: float
@@ -71,21 +75,37 @@ class NormalWishartParameters(Parameters):
     nu: float
     keep_inverse_W: bool=False
     W_inv: Optional[npt.NDArray[np_float]] = field(init=False)
+    stable: bool=True
 
     def __post_init__(self):
         d = self.mu0.shape[0]
+
+        if self.stable:
+            U, s, _ = np.linalg.svd(self.W)
+            # truncate the non-zero eigen values (dirty hack to make the
+            # reconstructed W to be somehow positive semi-definitive
+            s[s < 0] = 0.
+            self.W_chol = U @ np.diag(np.sqrt(s))
+            self.W = self.W_chol @ self.W_chol.T   # we do the cheating here as well
+
+            # https://math.stackexchange.com/a/2001054
+            self.logdet_W = np.log(np.maximum(
+                np.diag(self.W_chol), np.finfo('float').eps * 10
+            )).sum()
+
+        else:
+            self.W_chol = np.linalg.cholesky(self.W)
+
+            sign, self.logdet_W = np.linalg.slogdet(self.W)
+            if sign <= 0.:
+                raise ValueError(
+                    '[ERROR] log determinant of precision prior W is not defined!'
+                )
 
         if self.keep_inverse_W:
             self.W_inv = np.linalg.inv(self.W)
         else:
             self.W_inv = None
-
-        self.W_chol = np.linalg.cholesky(self.W)
-        sign, self.logdet_W = np.linalg.slogdet(self.W)
-        if sign <= 0.:
-            raise ValueError(
-                '[ERROR] log determinant of precision prior W is not defined!'
-            )
 
         # first natural parameter
         # (the second one Eq[eta2] is computed directly using